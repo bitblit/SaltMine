db426d459169cf9ac7ee46a007cb33e5eb8911cd
Chris Weiss <bitblit@gmail.com>
2021-05-26 13:45:45 -0600

    Refactor location

--- src/built-in/echo-processor.ts
+++ src/built-in/echo-processor.ts
@@ -1,5 +1,5 @@
-import { SaltMineEntry } from './salt-mine-entry';
 import { Logger } from '@bitblit/ratchet/dist/common';
+import { SaltMineEntry } from '../salt-mine-entry';
 
 export class EchoProcessor {
   public async handler(entry: SaltMineEntry): Promise<boolean> {
